400846eedd23d8ab0e765b0945bef957e09865ff
LarsO-bit <lars.olsson.94@outlook.com>
2026-02-27 15:28:57 +0100

    Skapat ett första utkast till sidans bakgrund

---
+++ background.js
@@ -0,0 +1,26 @@
+let offset = 0;
+
+function animateGround() {
+    offset += 0.6;
+    document.documentElement.style.setProperty("--ground-offset", `${offset}px`);
+    requestAnimationFrame(animateGround);
+}
+
+animateGround();
+
+const music = document.getElementById("bgMusic");
+const button = document.getElementById("musicToggle");
+
+let isPlaying = false;
+
+button.addEventListener("click", () => {
+    if (!isPlaying) {
+        music.play();
+        button.textContent = "⏸";
+        isPlaying = true;
+    } else {
+        music.pause();
+        button.textContent = "🎵";
+        isPlaying = false;
+    }
+});
