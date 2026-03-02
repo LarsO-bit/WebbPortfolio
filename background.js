8f704277ec57d7938c4c074efba89efc2fd9fda4
LarsO-bit <lars.olsson.94@outlook.com>
2026-03-02 13:38:05 +0100

    Button och text CSS

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 let offset = 0;
 
 function animateGround() {
-    offset += 0.6;
+    offset += 0.4;
     document.documentElement.style.setProperty("--ground-offset", `${offset}px`);
     requestAnimationFrame(animateGround);
 }
